ef2359f4cab2e3cbdf736c126d3b9424205dd24c
kishorchouhan <devkishorbwr@gmail.com>
2019-07-26 02:54:17 +0530

    fatch data from airtable

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -7,6 +7,10 @@ import Airtable from "airtable";
 import "./Calendar.css";
 import "./main.scss";
 
+var base = new Airtable({ apiKey: "keyEZdZTfWR3FMc3E" }).base(
+  "appfzSNTFcqqjf25D"
+);
+
 class Calendar extends Component {
   state = {
     allEvents: [
@@ -15,6 +19,35 @@ class Calendar extends Component {
     ],
     isLoading: true
   };
+
+  componentDidMount() {
+    base("stock data")
+      .select({
+        // Selecting the first 3 records in Grid view:
+        maxRecords: 3,
+        view: "Grid view"
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          // This function (`page`) will get called for each page of records.
+
+          records.forEach(function(record) {
+            console.log("Retrieved", record.get("id"));
+          });
+
+          // To fetch the next page of records, call `fetchNextPage`.
+          // If there are more records, `page` will get called again.
+          // If there are no more records, `done` will get called.
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        }
+      );
+  }
 
   handleDateClick = arg => {
     let element = arg.dayEl;
